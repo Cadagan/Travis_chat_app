d7a0c10db30fe8c803f15f2c8da900f6d62d3199
nbcl <nb@uc.cl>
2020-11-09 14:31:11 -0300

    Add log2.test.js, log3.test.js, Remove root.test.js

---
+++ test/log2.test.js
@@ -0,0 +1,15 @@
+// Log Test 2
+
+// Requirements
+var expect = require('chai').expect;
+
+// Log Test Start
+console.log("[Log Test 2]: Starting Test");
+describe("Log Test", function() {
+    it("provides a log", function(done) {
+        expect(1).to.be.equal(1);
+    });
+});
+
+// Completed
+console.log("[Log Test 2]: Done!");
