0dbb5fe97d39358acb0de2bccee82adbf341818c
kylevon <kbossonney1@uc.cl>
2020-11-09 17:57:34 -0300

    Changed some things so that the code compiles

--- frontend-t0/src/sessions.js
+++ frontend-t0/src/sessions.js
@@ -1,16 +1,11 @@
 //import Cookies from 'universal-cookie';
-//const cookies = new Cookies();
-//const cookies = require('universal-cookie');
 let sessionData = {sessionID: null, username: null};
 console.log('Initializing sessions');
 
 function setSessionID(newId) {
-  //console.log(cookies);
   console.log('Setting session id to: ' + newId);
   sessionData.sessionID = newId.sessionID;
   sessionData.token = newId.token;
-  //cookies.set('sessionID', newId.sessionID);
-  //cookies.set('token', newId.token);
 }
 function getSessionData() {
   return {
@@ -22,26 +17,20 @@ function getSessionData() {
 }
 
 function getSessionID() {
-  //return cookies.get('sessionID');
 }
 
 function setUsername(newUsername) {
-  //cookies.set('username', newUsername);
 }
 
 function getUsername() {
-  return cookies.get('username');
 }
 
 function getToken() {
-  //return cookies.get('token');
 }
 
 function setRole(role) {
-  //cookies.set('role', role);
 }
 function getRole() {
-  //return cookies.get('role');
 }
 
 module.exports = {
