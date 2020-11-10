051da9eff637f01ef5b8f6232916bed0f659824a
Christian Eilers <christian@eilers.cl>
2020-11-10 06:37:46 -0300

    Added new init file

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,10 +1,12 @@
 GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
 CREATE TABLE IF NOT EXISTS postgres.public.rooms
 (
-	id serial,
-	name text NOT NULL,
-	room_image text,
-	PRIMARY KEY (id)
+    id serial,
+    name text NOT NULL,
+    room_image text,
+    private boolean,
+    password varchar(50),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS postgres.public.messages
@@ -14,6 +16,8 @@ CREATE TABLE IF NOT EXISTS postgres.public.messages
     message text NOT NULL,
     roomid text NOT NULL,
     datetime timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    censured boolean,
+    originalmessage text,
     PRIMARY KEY (id)
 );
 
@@ -30,12 +34,9 @@ CREATE TABLE IF NOT EXISTS postgres.public.users
     CONSTRAINT unique_username UNIQUE (username)
 );
 
-
-
 ALTER TABLE public.messages
     OWNER to postgres;
 ALTER TABLE public.rooms
-	OWNER to postgres;
+    OWNER to postgres;
 ALTER TABLE public.users
-	OWNER to postgres;
-    
+    OWNER to postgres;
