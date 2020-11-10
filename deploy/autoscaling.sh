269a5163d9cb70dd9d3254e1b8a4d5710a3cc1ac
nbcl <nb@uc.cl>
2020-11-10 01:21:43 -0300

    Fix autoscaling in .travis.yml

---
+++ deploy/autoscaling.sh
@@ -0,0 +1,13 @@
+echo "Autoscaling Sleep 0/18"
+sleep 300
+echo "Autoscaling Sleep 3/18"
+sleep 300
+echo "Autoscaling Sleep 6/18"
+sleep 300
+echo "Autoscaling Sleep 9/18"
+sleep 300
+echo "Autoscaling Sleep 12/18"
+sleep 300
+echo "Autoscaling Sleep 15/18"
+sleep 300
+echo "Autoscaling Sleep 18/18"
