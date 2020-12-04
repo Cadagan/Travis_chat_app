463998024c4c5e8eda8c47cbab5a301e02c8a9a2
Christian Eilers <christian@eilers.cl>
2020-12-04 19:46:57 -0300

    Minor fixes to auth0

--- frontend-t0/src/config.js
+++ frontend-t0/src/config.js
@@ -1,5 +1,5 @@
 export default {
     "GOOGLE_CLIENT_ID": "622045053339-qknllrt2releujajsna65mast1jkjkdn.apps.googleusercontent.com",
     "AUTH0_DOMAIN": "dev-il48rxe3.us.auth0.com",
-    "AUTH0_CLIENT_ID": "PKl3Ea112AiNMZrs14DVM5CfLdaQK13J",
+    "AUTH0_CLIENT_ID": "YCSMf3DTeusPaqBViJT9uNTFoHJ0tsjR",
 }
